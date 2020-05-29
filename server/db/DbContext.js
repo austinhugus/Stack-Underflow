e1472a69c841cd344792bf537ce7aa76226e6c8c
Jacob Grow <jacobagrow@gmail.com>
2020-05-29 10:50:26 -0600

    Question server work-in-progress

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,8 +1,10 @@
 import ValueSchema from "../models/Value";
 import mongoose from "mongoose";
+import Question from "../models/Question"
 
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
+  Questions = mongoose.model("Question", Question)
 }
 
 export const dbContext = new DbContext();
---
+++ server/models/Question.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const Question = new Schema(
+  {
+    creator: { type: String, required: true },
+
+    title: { type: String, required: true },
+
+    body: { type: String, required: true }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+export default Question;
